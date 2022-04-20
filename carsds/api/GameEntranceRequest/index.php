5ab2e15abcda67105453c499b7163c91df6522d9
Rocket Programmer <rocketgamer580@gmail.com>
2022-04-20 16:12:15 -0500

    carsds: Push dummy APIs

---
+++ carsds/api/GameEntranceRequest/index.php
@@ -0,0 +1,8 @@
+<?php
+echo '<GameEntranceRequestResponse>';
+echo '<success>true</success>';
+echo '<queue>';
+echo '<can_enter_game>true</can_enter_game>';
+echo '</queue>';
+echo '</GameEntranceRequestResponse>';
+?>
---
+++ carsds/api/GenerateTokenRequest/index.php
@@ -0,0 +1,9 @@
+<?php
+echo '<GenerateTokenRequestResponse>';
+echo '<success>true</success>';
+echo '<queue>';
+echo '<can_enter_game>true</can_enter_game>';
+echo '</queue>';
+echo '<token>dev</token>';
+echo '</GenerateTokenRequestResponse>';
+?>
