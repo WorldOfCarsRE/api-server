dd064cf397716cffa4f5621699717781ea85553d
Rocket Programmer <rocketgamer580@gmail.com>
2022-04-23 13:43:54 -0500

    racecar: Flesh out more

--- racecar/Racecar.js
+++ racecar/Racecar.js
@@ -1,7 +1,47 @@
+ArrayCollection = global.ArrayCollection;
+
 class Racecar {
     constructor() {
+        this.chassisType = 0;
+        this.consumableSlot = 0;
+        this.active = false;
+        this.decalSlots = new ArrayCollection();
+        this.printableBackgroundId = 0;
+        this.totalMiles = 0;
+        this.costumeId = 0;
+        this.trophyItemList = new ArrayCollection();
+        this.modifyTime = new Date();
+        this.dashboardTintId = 0;
+        this.activeGearId = 0;
+        this.logoBackgroundColor = 0;
+        this.eyeColor = 0;
+        this.raceLevel = 0;
+        this.logoBackgroundId = 0;
+        this.carNumber = 0;
+        this.fans = 0;
+        this.gender = 0;
+        this.animationList = new ArrayCollection();
+        this.profileBackgroundId = 0;
+        this.userId = 0;
+        this.addonItemList = new ArrayCollection();
+        this.logoFontId = 0;
         this.playerId = 0;
-        this.carName = 'Car';
+        this.racecarId = 0;
+        this.sponsorList = new ArrayCollection();
+        this.racingPoints = 0;
+        this.danceSequenceList = new ArrayCollection();
+        this.wheel = 0;
+        this.color = 0;
+        this.consumableStack = 0;
+        this.dashboardTextureId = 0;
+        this.detailingId = 0;
+        this.raceSeries = 0;
+        this.activeSponsorId = 0;
+        this.logoFontColor = 0;
+        this.customItemList = new ArrayCollection();
+        this.tire = 0;
+        this.createTime = new Date();
+        this.carName = '';
     }
 }
 
