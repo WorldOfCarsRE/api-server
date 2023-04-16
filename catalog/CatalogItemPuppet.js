8cd59377a0dd002bb01612bd7617e7f9065f0ebb
Rocket <me@rocketprogrammer.me>
2023-04-16 11:38:27 -0500

    puppet: Fix up some values

--- catalog/CatalogItemPuppet.js
+++ catalog/CatalogItemPuppet.js
@@ -12,13 +12,13 @@ class CatalogItemPuppet extends CatalogItem {
     this.name = name
     this.repelRadius = 0
     this.chatBubbleColor = 0
-    this.idleAnimationId = 0
+    this.idleAnimationId = 33274 // From media/game/config/game.xml
     this.textOutlineColor = 0
-    this.nameHeight = 0
+    this.nameHeight = 120 // Currently set to CarSprite default
     this.animationIds = new ArrayCollection()
-    this.collisionHeight = 0
-    this.collisionWidth = 0
-    this.textColor = 0
+    this.collisionHeight = 67 // Currently set to ISOCar HEIGHT value
+    this.collisionWidth = 37 // Currently set to ISOCar WIDTH value
+    this.textColor = 16777215 // Currently set to value found in the CarSprite player setter function
     this.phraseIds = new ArrayCollection()
   }
 }
