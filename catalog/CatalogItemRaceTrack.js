bd442dc2bd7516b5f0afa03ec37026fae424ddff
Rocket <me@rocketprogrammer.me>
2023-05-13 12:33:37 -0500

    race-track: Comment

--- catalog/CatalogItemRaceTrack.js
+++ catalog/CatalogItemRaceTrack.js
@@ -4,6 +4,7 @@ const CatalogItemDungeon = global.CatalogItemDungeon
 ArrayCollection = global.ArrayCollection
 
 class CatalogItemRaceTrack extends CatalogItemDungeon {
+  // FIXME: This race track doesn't seem to load at the moment, for now we will use another map for experimenting.
   // constructor (proTrack = false, spClientScriptUrl = 'sp_ccs', titleUrl = 'car_f_gui_ttl_carburetorCounty_en_US.swf', scriptUrl = 'sp_ccs', mapUrl = 'car_w_trk_rsp_CarburetorCountySpeedway') {
   constructor (proTrack = false, spClientScriptUrl = 'sp_ccs', titleUrl = 'car_f_gui_ttl_twistinTailfinTrk_en_US.swf', scriptUrl = 'sp_ccs', mapUrl = 'car_w_trk_tfn_TwistinTailfinTrails', miniMapUrl = 'car_g_map_env_redhoodValley.swf') {
     super()
