38dfd95d65c7841559238c8f7bab2bf277635b96
Rocket <me@rocketprogrammer.me>
2023-05-13 12:32:50 -0500

    race-track: Almost loads

--- catalog/CatalogItemRaceTrack.js
+++ catalog/CatalogItemRaceTrack.js
@@ -4,7 +4,8 @@ const CatalogItemDungeon = global.CatalogItemDungeon
 ArrayCollection = global.ArrayCollection
 
 class CatalogItemRaceTrack extends CatalogItemDungeon {
-  constructor (proTrack = false, spClientScriptUrl = 'sp_ccs', titleUrl = 'car_f_gui_ttl_carburetorCounty_en_US.swf', scriptUrl = 'sp_ccs', mapUrl = 'car_w_trk_rsp_CarburetorCountySpeedway') {
+  // constructor (proTrack = false, spClientScriptUrl = 'sp_ccs', titleUrl = 'car_f_gui_ttl_carburetorCounty_en_US.swf', scriptUrl = 'sp_ccs', mapUrl = 'car_w_trk_rsp_CarburetorCountySpeedway') {
+  constructor (proTrack = false, spClientScriptUrl = 'sp_ccs', titleUrl = 'car_f_gui_ttl_twistinTailfinTrk_en_US.swf', scriptUrl = 'sp_ccs', mapUrl = 'car_w_trk_tfn_TwistinTailfinTrails', miniMapUrl = 'car_g_map_env_redhoodValley.swf') {
     super()
 
     this.proTrack = proTrack
@@ -13,6 +14,9 @@ class CatalogItemRaceTrack extends CatalogItemDungeon {
     this.titleUrl = titleUrl
     this.scriptUrl = scriptUrl
     this.mapUrl = mapUrl
+    this.miniMapUrl = miniMapUrl
+    this.photoFinishTrackUrl = 'car_f_gui_gam_photoFinish.swf'
+    this.physicsUrl = 'car_w_trk_tfn_twistinTailfin_SS_V1_phys.xml'
   }
 }
 
