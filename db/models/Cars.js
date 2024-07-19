2e2b351562c37489fbfd0c6230f796b508291c74
Little Cat <toontownlittlecat@gmail.com>
2024-07-19 03:53:08 -0300

    db: store friends list.

--- db/models/Cars.js
+++ db/models/Cars.js
@@ -8,7 +8,8 @@ const Cars = new mongoose.model('Cars', {
   ownerAccount: { type: String },
   dislId: { type: Number }, // OTP server Account object id
   playerId: { type: Number }, // OTP server DistributedCarPlayer object id
-  racecarId: { type: Number } // OTP server DistributedRaceCar object id
+  racecarId: { type: Number }, // OTP server DistributedRaceCar object id
+  friends: { type: Array } // Friends list (of Account object ids)
 })
 
 module.exports = Cars
